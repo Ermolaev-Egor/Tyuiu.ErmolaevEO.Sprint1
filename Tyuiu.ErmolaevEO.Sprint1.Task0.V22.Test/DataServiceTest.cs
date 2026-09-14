ab01c198a77cabd4dfeac2dfb540b9398275246e
lolkaya86 <jkfjermolaev@yandex.ru>
2026-09-14 12:45:01 +0500

    Add project files.

---
+++ Tyuiu.ErmolaevEO.Sprint1.Task0.V22.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.ErmolaevEO.Sprint1.Task0.V22.Lib;
+namespace Tyuiu.ErmolaevEO.Sprint1.Task0.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService sd = new DataService();
+            var res = sd.Calculate();
+            Assert.AreEqual(10, res);
+        }
+    }
+}
