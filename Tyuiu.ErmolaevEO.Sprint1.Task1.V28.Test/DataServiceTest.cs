dc26935aa10d738bdc12d988286cca007ade6b7c
lolkaya86 <jkfjermolaev@yandex.ru>
2026-09-16 10:21:30 +0500

    Task1

---
+++ Tyuiu.ErmolaevEO.Sprint1.Task1.V28.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.ErmolaevEO.Sprint1.Task1.V28.Lib;
+namespace Tyuiu.ErmolaevEO.Sprint1.Task1.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ Tyuiu.ErmolaevEO.Sprint1.Task1.V28/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.ErmolaevEO.Sprint1.Task1.V28.Lib;
+namespace Tyuiu.ErmolaevEO.Sprint1.Task1.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Ермолаев Е. О. | ИБКСб-26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Ермолаев Егор Олегович | ИБКСб-26-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле (x*2)/(1+x) и печатает его на экране.    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x;
+
+            Console.WriteLine("Введите значение X:");
+            x=Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x));
+            Console.ReadKey();
+        }
+    }
+}
